75e098545fae83b0e6b01cfbc9eb8b6723db41bc
Mohamad Al Mokdad <mohammad77.2002@gmail.com>
2024-11-10 21:20:45 +0200

    Fixed missing code

--- stock-pricing.py
+++ stock-pricing.py
@@ -1,32 +1,14 @@
-def max_profit(prices):
-    # Initialize an empty list to store calculated profit values
-    list_values=[]
+# Initialize the list with some integer values
+lista = [1, 2, 8, 4, 15, 20]
+max_diff = 0
 
-    
-    for i in range(1,len(prices)):# Loop through each selling price
-       
-       for j in range(i):# Loop through each selling price
-           
-           # Calculate profit as the difference between selling price and the values before (buying prices)
-           substracted_values=prices[i]-prices[j]
-           list_values.append(substracted_values)
-        
-    # Initialize max_profit with the first profit in list_values, then iterate through all values to find the maximum profit
-    max_profit = list_values[0]
-    for i in range(len(list_values)):
-        if max_profit < list_values[i]:
-            max_profit = list_values[i]
+# Outer loop to iterate through each element starting from the second element
+for i in range(1, len(lista)):
+    # Inner loop to iterate over all previous elements before the current element at index 'i'
+    for j in range(i):
+        # Calculate the difference between the element at index 'i' and the element at index 'j'
+        if lista[i] - lista[j] > max_diff:
+            max_diff = lista[i] - lista[j]
 
-    return max_profit
-  
-  
-
-
-
-
-
-
- 
-  
-
-  
+# Print the maximum difference found
+print(max_diff)
