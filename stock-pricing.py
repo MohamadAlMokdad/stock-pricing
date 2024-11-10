82e48eccebaeca570c507a42aeb7812022b4b03e
Mohamad Al Mokdad <mohammad77.2002@gmail.com>
2024-11-10 21:24:14 +0200

    fixed missing code

--- stock-pricing.py
+++ stock-pricing.py
@@ -1,14 +1,16 @@
-# Initialize the list with some integer values
-lista = [1, 2, 8, 4, 15, 20]
-max_diff = 0
+def max_profit(prices):
+    max_diff = 0
+    
+    # Outer loop to iterate through each price starting from the second element 
+    for i in range(1, len(prices)):
+        # Inner loop to iterate over all previous prices before the current price at index 'i'
+        for j in range(i):
+            # Calculate the difference between the price at index 'i' and the price at index 'j'
+           
+            if prices[i] - prices[j] > max_diff:
+                max_diff = prices[i] - prices[j]
+    
+    # Return the maximum profit found
+    return max_diff
 
-# Outer loop to iterate through each element starting from the second element
-for i in range(1, len(lista)):
-    # Inner loop to iterate over all previous elements before the current element at index 'i'
-    for j in range(i):
-        # Calculate the difference between the element at index 'i' and the element at index 'j'
-        if lista[i] - lista[j] > max_diff:
-            max_diff = lista[i] - lista[j]
 
-# Print the maximum difference found
-print(max_diff)
